7fd674f1998cd89f676a55ee0c7c0ed83f6265c2
Melissa Pereira Gnassingbe (Sigma) <melissa.p.gnassingbe@sigma-telecom-solutions.net>
2021-11-15 15:56:38 +0100

    fix constructors in vehicle class

--- xtasks/src/main/java/xtasks/backend/entities/Vehicle.java
+++ xtasks/src/main/java/xtasks/backend/entities/Vehicle.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 
 public class Vehicle {
 
-    VehicleStatus vehicleStatus;
+    Availability availability;
 
     @Id
     @GeneratedValue
@@ -20,13 +20,13 @@ public class Vehicle {
 
 
     public Vehicle(VehicleType vehicleType, VehicleFeatures vehicleFeatures) {
-        this.vehicleStatus = VehicleStatus.AVAILABLE;
+        this.availability = Availability.AVAILABLE;
         this.vehicleType = vehicleType;
         this.vehicleFeatures = vehicleFeatures;
     }
 
     public Vehicle(VehicleType vehicleType) {
-        this.vehicleStatus = VehicleStatus.AVAILABLE;
+        this.availability = Availability.AVAILABLE;
         this.vehicleType = vehicleType;
     }
 
@@ -37,19 +37,19 @@ public class Vehicle {
     @Override
     public String toString() {
         return "Vehicle: " +
-                "vehicleStatus=" + vehicleStatus +
+                "vehicleStatus=" + availability +
                 ", vehicleID=" + vehicleID +
                 ", vehicleType=" + vehicleType +
                 ", pictureURLs=" + Arrays.toString(pictureURLs) +
                 ", particularities='" + particularities + '\'';
     }
 
-    public VehicleStatus getVehicleStatus() {
-        return vehicleStatus;
+    public Availability getVehicleStatus() {
+        return availability;
     }
 
-    public void setVehicleStatus(VehicleStatus vehicleStatus) {
-        this.vehicleStatus = vehicleStatus;
+    public void setVehicleStatus(Availability availability) {
+        this.availability = availability;
     }
 
     public Long getVehicleID() {
