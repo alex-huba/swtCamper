ca2f240fa5b252a01077aa1cfeef883b21ec0dd3
Sabir Mammadov <sabir.mammadov@stud.uni-bamberg.de>
2021-11-17 16:06:50 +0100

    minor fix

--- swtcamper/src/main/java/swtcamper/javafx/controller/MainViewController.java
+++ swtcamper/src/main/java/swtcamper/javafx/controller/MainViewController.java
@@ -3,36 +3,17 @@ package swtcamper.javafx.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import swtcamper.backend.services.exceptions.GenericServiceException;
 
 @Component
 public class MainViewController {
-
-  @Autowired
-  private StatisticsViewController statisticsViewController;
-
-  @Autowired
-  private TaskAssignmentViewController taskAssignmentViewController;
-
-  @Autowired
-  private TodoListViewController todoListViewController;
 
   @FXML
   private void initialize() {
     reloadData();
   }
 
-  public void reloadData() {
-    try {
-      todoListViewController.reloadData();
-      statisticsViewController.reloadData();
-      taskAssignmentViewController.reloadData();
-    } catch (GenericServiceException e) {
-      handleException(e);
-    }
-  }
+  public void reloadData() {}
 
   public void handleExceptionMessage(String message) {
     Alert alert = new Alert(AlertType.ERROR);
