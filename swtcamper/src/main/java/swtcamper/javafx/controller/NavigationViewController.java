2216c08bb37d83c3366978cfa96cff897500cc82
Thomas Kretschmann <t.kretschmann@t-online.de>
2021-12-02 15:53:04 +0100

    removed vbox s.t. login button can be further down

--- swtcamper/src/main/java/swtcamper/javafx/controller/NavigationViewController.java
+++ swtcamper/src/main/java/swtcamper/javafx/controller/NavigationViewController.java
@@ -22,7 +22,7 @@ public class NavigationViewController {
   public AnchorPane navigationRoot;
 
   @FXML
-  public VBox navBarItems;
+  public AnchorPane navBarItems;
 
   @FXML
   public Button myOfferButton;
