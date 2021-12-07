cfdfc5f779c221cba41c837c7ac91ae754989d66
Melissa Pereira Gnassingbe (Sigma) <melissa.p.gnassingbe@sigma-telecom-solutions.net>
2021-12-07 18:15:40 +0100

    run npm autoformat

--- swtcamper/src/main/java/swtcamper/javafx/controller/PlaceOfferViewController.java
+++ swtcamper/src/main/java/swtcamper/javafx/controller/PlaceOfferViewController.java
@@ -2,10 +2,20 @@ package swtcamper.javafx.controller;
 
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.LongStringConverter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,16 +23,6 @@ import org.springframework.stereotype.Component;
 import swtcamper.api.contract.OfferDTO;
 import swtcamper.api.controller.OfferController;
 import swtcamper.backend.entities.VehicleType;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.paint.Color;
 
 @Component
 public class PlaceOfferViewController implements EventHandler<KeyEvent> {
@@ -178,39 +178,39 @@ public class PlaceOfferViewController implements EventHandler<KeyEvent> {
     double length = doubleStringConverter.fromString(lengthTextField.getText());
     double width = doubleStringConverter.fromString(widthTextField.getText());
     double height = doubleStringConverter.fromString(heightTextField.getText());
-      OfferDTO offerDTO = offerController.create(
-              longStringConverter.fromString(priceTextField.getText()),
-              minAgeCheckBox.isSelected(),
-              borderCrossingCheckBox.isSelected(),
-              depositCheckBox.isSelected(),
-              pictureURLs,
-              particularities,
-              typeBox.getValue(),
-              brandTextField.getText(),
-              modelTextField.getText(),
-              constructionYearTextField.getText(),
-              length,
-              width,
-              height,
-              engineTextField.getText(),
-              transmissionTextField.getText(),
-              Integer.parseInt(seatsTextField.getText()),
-              Integer.parseInt(bedsTextField.getText()),
-              roofTentCheckBox.isSelected(),
-              roofRackCheckBox.isSelected(),
-              bikeRackCheckBox.isSelected(),
-              showerCheckBox.isSelected(),
-              toiletCheckBox.isSelected(),
-              kitchenUnitCheckBox.isSelected(),
-              fridgeCheckBox.isSelected()
-      );
+    OfferDTO offerDTO = offerController.create(
+      longStringConverter.fromString(priceTextField.getText()),
+      minAgeCheckBox.isSelected(),
+      borderCrossingCheckBox.isSelected(),
+      depositCheckBox.isSelected(),
+      pictureURLs,
+      particularities,
+      typeBox.getValue(),
+      brandTextField.getText(),
+      modelTextField.getText(),
+      constructionYearTextField.getText(),
+      length,
+      width,
+      height,
+      engineTextField.getText(),
+      transmissionTextField.getText(),
+      Integer.parseInt(seatsTextField.getText()),
+      Integer.parseInt(bedsTextField.getText()),
+      roofTentCheckBox.isSelected(),
+      roofRackCheckBox.isSelected(),
+      bikeRackCheckBox.isSelected(),
+      showerCheckBox.isSelected(),
+      toiletCheckBox.isSelected(),
+      kitchenUnitCheckBox.isSelected(),
+      fridgeCheckBox.isSelected()
+    );
 
-      mainViewController.handleInformationMessage(
-              String.format("New offer \"%s\" has been created.", offerDTO.getID())
-      );
-      resetFields();
-      mainViewController.changeView("activeOffers");
-      mainViewController.reloadData();
+    mainViewController.handleInformationMessage(
+      String.format("New offer \"%s\" has been created.", offerDTO.getID())
+    );
+    resetFields();
+    mainViewController.changeView("activeOffers");
+    mainViewController.reloadData();
   }
 
   @FXML
@@ -218,9 +218,7 @@ public class PlaceOfferViewController implements EventHandler<KeyEvent> {
     mainViewController.changeView("activeOffers");
   }
 
-
-
-  private void validateOfferInput (KeyEvent event) {
+  private void validateOfferInput(KeyEvent event) {
     String source = "";
     source = event.getSource().toString();
     if (source.contains("priceTextField")) {
@@ -244,11 +242,23 @@ public class PlaceOfferViewController implements EventHandler<KeyEvent> {
         String inputPrice = priceTextField.getText();
         if (inputPrice.isEmpty() || !inputPrice.matches("[0-9]*")) {
           errorLabel.setText("Invalid price");
-          priceTextField.setBackground(new Background(new BackgroundFill(Color.LIGHTPINK, CornerRadii.EMPTY, Insets.EMPTY)));
+          priceTextField.setBackground(
+            new Background(
+              new BackgroundFill(
+                Color.LIGHTPINK,
+                CornerRadii.EMPTY,
+                Insets.EMPTY
+              )
+            )
+          );
           isPriceOk = false;
         } else {
           errorLabel.setText("");
-          priceTextField.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+          priceTextField.setBackground(
+            new Background(
+              new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)
+            )
+          );
           isPriceOk = true;
         }
         break;
@@ -256,11 +266,23 @@ public class PlaceOfferViewController implements EventHandler<KeyEvent> {
         String inputBrand = brandTextField.getText();
         if (inputBrand.isEmpty()) {
           errorLabel.setText("Invalid brand");
-          brandTextField.setBackground(new Background(new BackgroundFill(Color.LIGHTPINK, CornerRadii.EMPTY, Insets.EMPTY)));
+          brandTextField.setBackground(
+            new Background(
+              new BackgroundFill(
+                Color.LIGHTPINK,
+                CornerRadii.EMPTY,
+                Insets.EMPTY
+              )
+            )
+          );
           isBrandOk = false;
         } else {
           errorLabel.setText("");
-          brandTextField.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+          brandTextField.setBackground(
+            new Background(
+              new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)
+            )
+          );
           isBrandOk = true;
         }
         break;
@@ -268,11 +290,23 @@ public class PlaceOfferViewController implements EventHandler<KeyEvent> {
         String inputModel = modelTextField.getText();
         if (inputModel.isEmpty()) {
           errorLabel.setText("Invalid model");
-          modelTextField.setBackground(new Background(new BackgroundFill(Color.LIGHTPINK, CornerRadii.EMPTY, Insets.EMPTY)));
+          modelTextField.setBackground(
+            new Background(
+              new BackgroundFill(
+                Color.LIGHTPINK,
+                CornerRadii.EMPTY,
+                Insets.EMPTY
+              )
+            )
+          );
           isModelOk = false;
         } else {
           errorLabel.setText("");
-          modelTextField.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+          modelTextField.setBackground(
+            new Background(
+              new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)
+            )
+          );
           isModelOk = true;
         }
         break;
@@ -280,11 +314,23 @@ public class PlaceOfferViewController implements EventHandler<KeyEvent> {
         String inputSeats = seatsTextField.getText();
         if (inputSeats.isEmpty() || !inputSeats.matches("[0-9]*")) {
           errorLabel.setText("Invalid seats");
-          seatsTextField.setBackground(new Background(new BackgroundFill(Color.LIGHTPINK, CornerRadii.EMPTY, Insets.EMPTY)));
+          seatsTextField.setBackground(
+            new Background(
+              new BackgroundFill(
+                Color.LIGHTPINK,
+                CornerRadii.EMPTY,
+                Insets.EMPTY
+              )
+            )
+          );
           isSeatsOk = false;
         } else {
           errorLabel.setText("");
-          seatsTextField.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+          seatsTextField.setBackground(
+            new Background(
+              new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)
+            )
+          );
           isSeatsOk = true;
         }
         break;
@@ -292,11 +338,23 @@ public class PlaceOfferViewController implements EventHandler<KeyEvent> {
         String inputBeds = bedsTextField.getText();
         if (inputBeds.isEmpty() || !inputBeds.matches("[0-9]*")) {
           errorLabel.setText("Invalid beds");
-          bedsTextField.setBackground(new Background(new BackgroundFill(Color.LIGHTPINK, CornerRadii.EMPTY, Insets.EMPTY)));
+          bedsTextField.setBackground(
+            new Background(
+              new BackgroundFill(
+                Color.LIGHTPINK,
+                CornerRadii.EMPTY,
+                Insets.EMPTY
+              )
+            )
+          );
           isBedsOk = false;
         } else {
           errorLabel.setText("");
-          bedsTextField.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+          bedsTextField.setBackground(
+            new Background(
+              new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)
+            )
+          );
           isBedsOk = true;
         }
         break;
@@ -306,7 +364,6 @@ public class PlaceOfferViewController implements EventHandler<KeyEvent> {
     } else {
       placeOfferButton.setDisable(true);
     }
-
   }
 
   public void importFileChooserAction(ActionEvent actionEvent) {}
