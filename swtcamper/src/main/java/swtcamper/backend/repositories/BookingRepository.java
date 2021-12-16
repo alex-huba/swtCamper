423882420db704f4a7b5215f421c63e3ef10354c
Aaron Hissting <aaron-joachim.hissting@stud.uni-bamberg.de>
2021-12-16 20:16:15 +0100

    Implemented offerStillAvailable(long offerID, long bookingID)
    Überprüft beim Re-Aktivieren von einem Booking, ob das Offer im ursprünglich angegebenen Zeitraum noch verfügbar ist

--- swtcamper/src/main/java/swtcamper/backend/repositories/BookingRepository.java
+++ swtcamper/src/main/java/swtcamper/backend/repositories/BookingRepository.java
@@ -1,12 +1,11 @@
 package swtcamper.backend.repositories;
 
+import java.util.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import swtcamper.backend.entities.Booking;
 
-import java.util.List;
-
 @Repository
 public interface BookingRepository extends CrudRepository<Booking, Long> {
-    List<Booking> findAll();
+  List<Booking> findAll();
 }
--- swtcamper/src/main/java/swtcamper/backend/services/BookingService.java
+++ swtcamper/src/main/java/swtcamper/backend/services/BookingService.java
@@ -1,70 +1,94 @@
 package swtcamper.backend.services;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import swtcamper.backend.entities.Booking;
-import swtcamper.backend.entities.Offer;
-import swtcamper.backend.entities.User;
-import swtcamper.backend.repositories.BookingRepository;
-import swtcamper.backend.repositories.OfferRepository;
-
 import java.time.LocalDate;
-import java.time.Period;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import swtcamper.backend.entities.Booking;
+import swtcamper.backend.entities.Offer;
+import swtcamper.backend.repositories.BookingRepository;
+import swtcamper.backend.repositories.OfferRepository;
 
 @Service
 public class BookingService {
 
-    @Autowired
-    private OfferRepository offerRepository;
+  @Autowired
+  private OfferRepository offerRepository;
 
-    @Autowired
-    private BookingRepository bookingRepository;
+  @Autowired
+  private BookingRepository bookingRepository;
 
-    public List getBookedDays(long offerID) {
-        List<LocalDate> bookedDays = new ArrayList<>();
+  public List getBookedDays(long offerID) {
+    List<LocalDate> bookedDays = new ArrayList<>();
 
-        Optional<Offer> offerResponse = offerRepository.findById(offerID);
-        Offer offer = offerResponse.get();
-        ArrayList<Long> bookingIDs = offer.getBookings();
-        Iterable<Booking> bookings = bookingRepository.findAllById(bookingIDs);
+    Optional<Offer> offerResponse = offerRepository.findById(offerID);
+    Offer offer = offerResponse.get();
+    ArrayList<Long> bookingIDs = offer.getBookings();
+    Iterable<Booking> bookings = bookingRepository.findAllById(bookingIDs);
 
-        for (Booking booking : bookings) {
-            LocalDate startDate = booking.getStartDate();
-            LocalDate endDate = booking.getEndDate();
-            long amountOfDays = ChronoUnit.DAYS.between(startDate, endDate);
-            for (int i = 0; i <= amountOfDays; i++) {
-                bookedDays.add(startDate.plus(i, ChronoUnit.DAYS));
-            }
-        }
-        return bookedDays;
+    for (Booking booking : bookings) {
+      LocalDate startDate = booking.getStartDate();
+      LocalDate endDate = booking.getEndDate();
+      long amountOfDays = ChronoUnit.DAYS.between(startDate, endDate);
+      for (int i = 0; i <= amountOfDays; i++) {
+        bookedDays.add(startDate.plus(i, ChronoUnit.DAYS));
+      }
+    }
+    return bookedDays;
+  }
+
+  public ArrayList<Long> getAvailableOffers(
+    LocalDate startDate,
+    LocalDate endDate
+  ) {
+    // Liste aller angefragten Tage
+    List<LocalDate> requestedDays = new ArrayList<>();
+    long amountOfDays = ChronoUnit.DAYS.between(startDate, endDate);
+    for (int i = 0; i <= amountOfDays; i++) {
+      requestedDays.add(startDate.plus(i, ChronoUnit.DAYS));
     }
 
-    public ArrayList<Long> getAvailableOffers(LocalDate startDate, LocalDate endDate) {
-        // Liste aller angefragten Tage
-        List<LocalDate> requestedDays = new ArrayList<>();
-        long amountOfDays = ChronoUnit.DAYS.between(startDate, endDate);
-        for (int i = 0; i <= amountOfDays; i++) {
-            requestedDays.add(startDate.plus(i, ChronoUnit.DAYS));
-        }
+    // Liste die returned wird
+    ArrayList<Long> offerIDs = new ArrayList<>();
 
-        // Liste die returned wird
-        ArrayList<Long> offerIDs = new ArrayList<>();
-
-        // Alle Offers holen und die angefragten Tage gegen die bereits gebuchten Tage gegenchecken
-        List<Offer> offerResponse = offerRepository.findAll();
-        for (Offer offer : offerResponse) {
-            List<LocalDate> bookedDays = getBookedDays(offer.getOfferID());
-            for (LocalDate requestedDay : requestedDays) {
-                if(!bookedDays.contains(requestedDay)) {
-                  offerIDs.add(offer.getOfferID());
-                };
-            }
+    // Alle Offers holen und die angefragten Tage gegen die bereits gebuchten Tage gegenchecken
+    List<Offer> offerResponse = offerRepository.findAll();
+    for (Offer offer : offerResponse) {
+      List<LocalDate> bookedDays = getBookedDays(offer.getOfferID());
+      for (LocalDate requestedDay : requestedDays) {
+        if (!bookedDays.contains(requestedDay)) {
+          offerIDs.add(offer.getOfferID());
         }
-        return offerIDs;
+      }
     }
+    return offerIDs;
+  }
+
+  public boolean offerStillAvailable(long offerID, long bookingID) {
+    Optional<Offer> offerResponse = offerRepository.findById(offerID);
+    Offer offer = offerResponse.get();
+
+    Optional<Booking> bookingResponse = bookingRepository.findById(bookingID);
+    Booking booking = bookingResponse.get();
+
+    LocalDate startDate = booking.getStartDate();
+    LocalDate endDate = booking.getEndDate();
+
+    List<LocalDate> requestedDays = new ArrayList<>();
+    long amountOfDays = ChronoUnit.DAYS.between(startDate, endDate);
+    for (int i = 0; i <= amountOfDays; i++) {
+      requestedDays.add(startDate.plus(i, ChronoUnit.DAYS));
+    }
+
+    List<LocalDate> bookedDays = getBookedDays(offerID);
+    for (LocalDate requestedDay : requestedDays) {
+      if (bookedDays.contains(requestedDay)) {
+        return false;
+      }
+    }
+    return true;
+  }
 }
-
