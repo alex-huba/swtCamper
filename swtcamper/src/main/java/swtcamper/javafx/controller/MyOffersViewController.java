72ac12e5d4d65fbc58bae9013486658fa5e72d5a
Thomas Kretschmann <t.kretschmann@t-online.de>
2021-12-22 12:59:03 +0100

    added cards to myOffers

--- swtcamper/src/main/java/swtcamper/javafx/controller/MyOffersViewController.java
+++ swtcamper/src/main/java/swtcamper/javafx/controller/MyOffersViewController.java
@@ -1,13 +1,21 @@
 package swtcamper.javafx.controller;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 import java.util.Optional;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.ListView;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import swtcamper.api.contract.OfferDTO;
@@ -16,7 +24,6 @@ import swtcamper.backend.services.exceptions.GenericServiceException;
 
 @Component
 public class MyOffersViewController {
-
   private OfferDTO selectedOffer;
 
   @Autowired
@@ -32,22 +39,112 @@ public class MyOffersViewController {
   private OfferViewController offerViewController;
 
   @FXML
-  public ListView<OfferDTO> offersList;
+  public ScrollPane offersList;
+
+  @FXML
+  public VBox offerListRoot;
 
   @FXML
   public void initialize() throws GenericServiceException {
     reloadData();
-    offersList.setOnMouseClicked(click -> {
-      OfferDTO selectedItem = offersList.getSelectionModel().getSelectedItem();
-      //Listener for right click
-      if (click.isSecondaryButtonDown()) {
-        //ignore
-      }
-      //Listener for double click
-      if (click.getClickCount() == 2) {
-        showInfoAlert(selectedItem);
-      }
-    });
+  }
+
+  private void setData(List<OfferDTO> offers) throws GenericServiceException {
+    offerListRoot.getChildren().clear();
+
+    Label header = new Label((offers.size() == 0 ? "Keine passenden" : "Passende") + " Angebote für Sie");
+    header.setStyle("-fx-font-size: 30");
+    offerListRoot.getChildren().add(header);
+
+    for (OfferDTO offer : offers) {
+      VBox root = new VBox();
+      root.setStyle(
+              "-fx-background-color: #c9dfce; -fx-background-radius: 20px"
+      );
+      root.setEffect(new DropShadow(4d, 0d, +6d, Color.BLACK));
+
+      Image image = offer.getOfferedObject().getPictureURLs().length > 0 ? new Image(offer.getOfferedObject().getPictureURLs()[0]) : new Image("/pictures/noImg.png");
+
+      HBox offerDetails = new HBox();
+
+      ImageView thumbnail = new ImageView(image);
+      thumbnail.setFitHeight(80);
+      thumbnail.setFitWidth(90);
+      offerDetails.getChildren().add(thumbnail);
+
+      Label titleLabel = new Label(offer.getTitle());
+      titleLabel.setStyle(
+              "-fx-font-size: 35; -fx-font-family: \"Arial Rounded MT Bold\"; -fx-text-fill: #040759"
+      );
+
+      Label locationLabel = new Label("Abholort: " + offer.getLocation());
+      locationLabel.setStyle(
+              "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
+      );
+
+      Label priceLabel = new Label(
+              "Preis pro Tag: € " + Long.toString(offer.getPrice())
+      );
+      priceLabel.setStyle(
+              "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
+      );
+
+      Label brandLabel = new Label(
+              "Marke: " + offer.getOfferedObject().getVehicleFeatures().getMake()
+      );
+      brandLabel.setStyle(
+              "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
+      );
+
+      Label modelLabel = new Label(
+              "Modell: " + offer.getOfferedObject().getVehicleFeatures().getModel()
+      );
+      modelLabel.setStyle(
+              "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
+      );
+
+      VBox detailsVBox = new VBox();
+      detailsVBox.setAlignment(Pos.TOP_CENTER);
+      detailsVBox.getChildren().add(titleLabel);
+
+      VBox locationPriceBrandModelBox = new VBox();
+      locationPriceBrandModelBox.setStyle("-fx-padding: 0 0 0 30");
+      locationPriceBrandModelBox.getChildren().add(locationLabel);
+      locationPriceBrandModelBox.getChildren().add(priceLabel);
+      locationPriceBrandModelBox.getChildren().add(brandLabel);
+      locationPriceBrandModelBox.getChildren().add(modelLabel);
+      detailsVBox.getChildren().add(locationPriceBrandModelBox);
+
+      Button moreBtn = new Button("Mehr Information");
+      moreBtn.setStyle(
+              "-fx-background-radius: 8; -fx-background-color: #e79e69"
+      );
+      moreBtn.setOnAction(
+              new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent event) {
+                  try {
+                    mainViewController.changeView("viewOffer");
+                    offerViewController.initialize(offer, false);
+                  } catch (GenericServiceException e) {
+                    e.printStackTrace();
+                  }
+                }
+              }
+      );
+
+      HBox btnBox = new HBox();
+      btnBox.setAlignment(Pos.TOP_RIGHT);
+      btnBox.setStyle("-fx-padding: 0 30 30 0");
+      btnBox.getChildren().add(moreBtn);
+      detailsVBox.getChildren().add(btnBox);
+
+      offerDetails.setHgrow(detailsVBox, Priority.ALWAYS);
+      offerDetails.getChildren().add(detailsVBox);
+
+      root.getChildren().add(offerDetails);
+      offerListRoot.getChildren().add(root);
+    }
   }
 
   @FXML
@@ -55,59 +152,8 @@ public class MyOffersViewController {
     mainViewController.changeView("placeOffer");
   }
 
-  @FXML
-  public void viewOfferAction() throws GenericServiceException {
-    OfferDTO selectedOffer = offersList.getSelectionModel().getSelectedItem();
-    if (selectedOffer != null) {
-      mainViewController.changeView("viewOffer");
-      offerViewController.initialize(selectedOffer, false);
-    } else {
-      showSelectOfferFirstInfo();
-    }
-  }
-
-  private void showSelectOfferFirstInfo() {
-    Alert alert = new Alert(
-      Alert.AlertType.WARNING,
-      "Bitte wähle erst ein Angebot aus der Liste"
-    );
-    alert.showAndWait();
-  }
-
-  @FXML
-  public void updateOfferAction() throws GenericServiceException {
-    OfferDTO selectedOffer = offersList.getSelectionModel().getSelectedItem();
-    if (selectedOffer != null) {
-      mainViewController.changeView("placeOffer");
-      modifyOfferViewController.initialize(selectedOffer);
-    } else {
-      showSelectOfferFirstInfo();
-    }
-  }
-
-  @FXML
-  public void removeOfferAction() throws GenericServiceException {
-    OfferDTO selectedOffer = offersList.getSelectionModel().getSelectedItem();
-    if (selectedOffer != null) {
-      Alert confirmDelete = new Alert(
-        Alert.AlertType.CONFIRMATION,
-        "Willst du dieses Angebot wirklich entfernen?"
-      );
-      Optional<ButtonType> result = confirmDelete.showAndWait();
-
-      if (result.isPresent() && result.get() == ButtonType.OK) {
-        offerController.delete(selectedOffer.getID());
-        reloadData();
-      }
-    } else {
-      showSelectOfferFirstInfo();
-    }
-  }
-
   public void reloadData() throws GenericServiceException {
-    offersList.setItems(
-      FXCollections.observableArrayList(offerController.offers())
-    );
+    setData(offerController.offers());
   }
 
   private void showInfoAlert(OfferDTO offerItem) {
