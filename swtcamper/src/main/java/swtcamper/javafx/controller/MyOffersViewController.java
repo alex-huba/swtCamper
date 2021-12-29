01c56dddb979e58e86bee3a1378cb10c5f0d6a41
Thomas Kretschmann <t.kretschmann@t-online.de>
2021-12-29 16:43:05 +0100

    ran autoformat

--- swtcamper/src/main/java/swtcamper/javafx/controller/MyOffersViewController.java
+++ swtcamper/src/main/java/swtcamper/javafx/controller/MyOffersViewController.java
@@ -1,18 +1,14 @@
 package swtcamper.javafx.controller;
 
 import java.util.Optional;
-
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
-import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import swtcamper.api.contract.OfferDTO;
@@ -45,16 +41,17 @@ public class MyOffersViewController {
   public VBox offerListRoot;
 
   @FXML
-  public void initialize() {
-  }
+  public void initialize() {}
 
   public void reloadData() throws GenericServiceException {
     offerListRoot.getChildren().clear();
 
-    for (OfferDTO offer : offerController.getOffersCreatedByUser(userController.getLoggedInUser())) {
+    for (OfferDTO offer : offerController.getOffersCreatedByUser(
+      userController.getLoggedInUser()
+    )) {
       VBox root = new VBox();
       root.setStyle(
-              "-fx-background-color: #c9dfce; -fx-background-radius: 20px"
+        "-fx-background-color: #c9dfce; -fx-background-radius: 20px"
       );
 
       Image image;
@@ -74,33 +71,33 @@ public class MyOffersViewController {
 
       Label titleLabel = new Label(offer.getTitle());
       titleLabel.setStyle(
-              "-fx-font-size: 35; -fx-font-family: \"Arial Rounded MT Bold\"; -fx-text-fill: #040759"
+        "-fx-font-size: 35; -fx-font-family: \"Arial Rounded MT Bold\"; -fx-text-fill: #040759"
       );
 
       Label locationLabel = new Label("Abholort: " + offer.getLocation());
       locationLabel.setStyle(
-              "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
+        "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
       );
 
       Label priceLabel = new Label(
-              "Preis pro Tag: € " + Long.toString(offer.getPrice())
+        "Preis pro Tag: € " + Long.toString(offer.getPrice())
       );
       priceLabel.setStyle(
-              "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
+        "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
       );
 
       Label brandLabel = new Label(
-              "Marke: " + offer.getOfferedObject().getVehicleFeatures().getMake()
+        "Marke: " + offer.getOfferedObject().getVehicleFeatures().getMake()
       );
       brandLabel.setStyle(
-              "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
+        "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
       );
 
       Label modelLabel = new Label(
-              "Modell: " + offer.getOfferedObject().getVehicleFeatures().getModel()
+        "Modell: " + offer.getOfferedObject().getVehicleFeatures().getModel()
       );
       modelLabel.setStyle(
-              "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
+        "-fx-font-size: 20; -fx-font-family: \"Arial Rounded MT Bold\";"
       );
 
       VBox detailsVBox = new VBox();
@@ -137,24 +134,22 @@ public class MyOffersViewController {
       removeBtn.getStyleClass().add("bg-danger");
       removeBtn.setOnAction(event -> {
         Alert confirmDelete = new Alert(
-                Alert.AlertType.CONFIRMATION,
-                "Willst du dieses Angebot wirklich entfernen?"
+          Alert.AlertType.CONFIRMATION,
+          "Willst du dieses Angebot wirklich entfernen?"
         );
         Optional<ButtonType> result = confirmDelete.showAndWait();
 
         if (result.isPresent() && result.get() == ButtonType.OK) {
           try {
             offerController.delete(offer.getID());
-          } catch (GenericServiceException ignore) {
-          }
+          } catch (GenericServiceException ignore) {}
           try {
             reloadData();
-          } catch (GenericServiceException ignore) {
-          }
+          } catch (GenericServiceException ignore) {}
         }
       });
 
-      HBox btnBox = new HBox(moreBtn,updateBtn,removeBtn);
+      HBox btnBox = new HBox(moreBtn, updateBtn, removeBtn);
       btnBox.setAlignment(Pos.TOP_RIGHT);
       btnBox.setStyle("-fx-padding: 0 30 30 0");
       btnBox.setSpacing(5);
