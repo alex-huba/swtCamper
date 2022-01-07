165eefa87fb886cd0ac3ed694a6e3469bf42157f
Thomas Kretschmann <t.kretschmann@t-online.de>
2022-01-07 11:29:02 +0100

    added approveController, but: approve deal or approve new user?

---
+++ swtcamper/src/main/java/swtcamper/javafx/controller/ApproveDealViewController.java
@@ -0,0 +1,17 @@
+package swtcamper.javafx.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ApproveDealViewController {
+
+    @FXML
+    public VBox toApproveListView;
+
+    @FXML
+    public void initialize() {
+
+    }
+}
