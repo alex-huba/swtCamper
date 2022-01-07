b05e343d917152150cd62f8ba4131608b3af908f
Thomas Kretschmann <t.kretschmann@t-online.de>
2022-01-07 14:35:12 +0100

    fixed pipeline

--- swtcamper/src/main/java/swtcamper/javafx/controller/OfferViewController.java
+++ swtcamper/src/main/java/swtcamper/javafx/controller/OfferViewController.java
@@ -9,6 +9,7 @@ import javafx.scene.control.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.util.converter.DoubleStringConverter;
 import javafx.util.converter.LongStringConverter;
 import org.springframework.beans.factory.annotation.Autowired;
