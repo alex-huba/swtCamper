d9aade6ca7c08164e0ede1030f209bca4acfa29a
Thomas Kretschmann <t.kretschmann@t-online.de>
2022-01-11 14:31:13 +0100

    temp

--- swtcamper/src/main/java/swtcamper/javafx/controller/ExcludeRenterViewController.java
+++ swtcamper/src/main/java/swtcamper/javafx/controller/ExcludeRenterViewController.java
@@ -1,6 +1,8 @@
 package swtcamper.javafx.controller;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,6 +19,9 @@ public class ExcludeRenterViewController {
     public VBox excludedRentersVBox;
 
     @FXML
+    public TextField findUsersTextField;
+
+    @FXML
     public VBox userResultsVBox;
 
     @FXML
@@ -29,5 +34,9 @@ public class ExcludeRenterViewController {
         for(User excludedUser : userController.getLoggedInUser().getExcludedRenters()) {
 
         }
+    }
+
+    public void findUsers(ActionEvent actionEvent) {
+        for(User user : userController.)
     }
 }
