51aacc79e6df2f73a18c2faff9cc4e76baa3358e
oleksandr.huba <oleksandr.huba@stud.uni-bamberg.de>
2022-01-17 23:25:49 +0100

    autoformat

--- swtcamper/src/test/java/swtcamper/backend/services/PictureServiceTest.java
+++ swtcamper/src/test/java/swtcamper/backend/services/PictureServiceTest.java
@@ -20,118 +20,118 @@ import swtcamper.backend.repositories.PictureRepository;
 @RunWith(MockitoJUnitRunner.class)
 public class PictureServiceTest {
 
-    @InjectMocks
-    private PictureService pictureServiceUnderTest;
+  @InjectMocks
+  private PictureService pictureServiceUnderTest;
 
-    @Mock
-    private ModelMapper modelMapper;
+  @Mock
+  private ModelMapper modelMapper;
 
-    @Mock
-    private PictureRepository pictureRepository;
+  @Mock
+  private PictureRepository pictureRepository;
 
-    private PictureDTO giveMeTestPictureDTO() {
-        PictureDTO testPictureDTO = new PictureDTO(1001, 505, "/path/folder/");
-        return testPictureDTO;
-    }
+  private PictureDTO giveMeTestPictureDTO() {
+    PictureDTO testPictureDTO = new PictureDTO(1001, 505, "/path/folder/");
+    return testPictureDTO;
+  }
 
-    private PictureDTO giveMeOneMoreTestPictureDTO() {
-        PictureDTO testPictureDTO = new PictureDTO(2002, 808, "/abc/directory/");
-        return testPictureDTO;
-    }
+  private PictureDTO giveMeOneMoreTestPictureDTO() {
+    PictureDTO testPictureDTO = new PictureDTO(2002, 808, "/abc/directory/");
+    return testPictureDTO;
+  }
 
-    private Picture giveMeTestPicture() {
-        Picture testPicture = new Picture();
-        testPicture.setPictureID(1001);
-        testPicture.setVehicleID(505);
-        testPicture.setPath("/path/folder/");
-        return testPicture;
-    }
+  private Picture giveMeTestPicture() {
+    Picture testPicture = new Picture();
+    testPicture.setPictureID(1001);
+    testPicture.setVehicleID(505);
+    testPicture.setPath("/path/folder/");
+    return testPicture;
+  }
 
-    private Picture giveMeOneMoreTestPicture() {
-        Picture testPicture = new Picture();
-        testPicture.setPictureID(2002);
-        testPicture.setVehicleID(808);
-        testPicture.setPath("/abc/directory/");
-        return testPicture;
-    }
+  private Picture giveMeOneMoreTestPicture() {
+    Picture testPicture = new Picture();
+    testPicture.setPictureID(2002);
+    testPicture.setVehicleID(808);
+    testPicture.setPath("/abc/directory/");
+    return testPicture;
+  }
 
-    private List<Picture> giveMeListOfPictures() {
-        List<Picture> pictures = new ArrayList<>();
-        pictures.add(giveMeTestPicture());
-        pictures.add(giveMeOneMoreTestPicture());
-        return pictures;
-    }
+  private List<Picture> giveMeListOfPictures() {
+    List<Picture> pictures = new ArrayList<>();
+    pictures.add(giveMeTestPicture());
+    pictures.add(giveMeOneMoreTestPicture());
+    return pictures;
+  }
 
-    private List<PictureDTO> giveMeListOfPicturesDTO() {
-        List<PictureDTO> pictureDTOS = new ArrayList<>();
-        pictureDTOS.add(giveMeTestPictureDTO());
-        pictureDTOS.add(giveMeOneMoreTestPictureDTO());
-        return pictureDTOS;
-    }
+  private List<PictureDTO> giveMeListOfPicturesDTO() {
+    List<PictureDTO> pictureDTOS = new ArrayList<>();
+    pictureDTOS.add(giveMeTestPictureDTO());
+    pictureDTOS.add(giveMeOneMoreTestPictureDTO());
+    return pictureDTOS;
+  }
 
-    private void mockPictureDTOToPicture(PictureDTO pictureDTO) {
-        doReturn(giveMeTestPicture()).when(modelMapper).pictureDTOToPicture(any());
-    }
+  private void mockPictureDTOToPicture(PictureDTO pictureDTO) {
+    doReturn(giveMeTestPicture()).when(modelMapper).pictureDTOToPicture(any());
+  }
 
-    private void mockPictureToPictureDTO(Picture picture) {
-        doReturn(giveMeTestPictureDTO())
-                .when(modelMapper)
-                .pictureToPictureDTO(any());
-    }
+  private void mockPictureToPictureDTO(Picture picture) {
+    doReturn(giveMeTestPictureDTO())
+      .when(modelMapper)
+      .pictureToPictureDTO(any());
+  }
 
-    @BeforeEach
-    public void saveTestPicture() {
-        when(pictureRepository.save(giveMeTestPicture()))
-                .thenReturn(giveMeTestPicture());
-    }
+  @BeforeEach
+  public void saveTestPicture() {
+    when(pictureRepository.save(giveMeTestPicture()))
+      .thenReturn(giveMeTestPicture());
+  }
 
-    @Test
-    public void whenCreatePictureItShouldReturnPictureDTO() {
-        // Given
-        mockPictureDTOToPicture(giveMeTestPictureDTO());
-        mockPictureToPictureDTO(giveMeTestPicture());
+  @Test
+  public void whenCreatePictureItShouldReturnPictureDTO() {
+    // Given
+    mockPictureDTOToPicture(giveMeTestPictureDTO());
+    mockPictureToPictureDTO(giveMeTestPicture());
 
-        // When
-        pictureServiceUnderTest.create(giveMeTestPictureDTO());
+    // When
+    pictureServiceUnderTest.create(giveMeTestPictureDTO());
 
-        ArgumentCaptor<Picture> pictureArgumentCaptor = ArgumentCaptor.forClass(
-                Picture.class
-        );
+    ArgumentCaptor<Picture> pictureArgumentCaptor = ArgumentCaptor.forClass(
+      Picture.class
+    );
 
-        verify(pictureRepository, times(1)).save(pictureArgumentCaptor.capture());
+    verify(pictureRepository, times(1)).save(pictureArgumentCaptor.capture());
 
-        // Then
-        assertEquals(
-                giveMeTestPicture().getPictureID(),
-                pictureArgumentCaptor.getValue().getPictureID()
-        );
-    }
+    // Then
+    assertEquals(
+      giveMeTestPicture().getPictureID(),
+      pictureArgumentCaptor.getValue().getPictureID()
+    );
+  }
 
-    @Test
-    public void whenGetPicturesItShouldReturnPicturesDTO() {
-        when(pictureRepository.findAll()).thenReturn(giveMeListOfPictures());
-        when(modelMapper.picturesToPictureDTOs(any()))
-                .thenReturn(giveMeListOfPicturesDTO());
+  @Test
+  public void whenGetPicturesItShouldReturnPicturesDTO() {
+    when(pictureRepository.findAll()).thenReturn(giveMeListOfPictures());
+    when(modelMapper.picturesToPictureDTOs(any()))
+      .thenReturn(giveMeListOfPicturesDTO());
 
-        List<PictureDTO> actual = pictureServiceUnderTest.getPicturesForVehicle(
-                giveMeTestPictureDTO().getVehicleID()
-        );
-        PictureDTO actualPicture = actual.get(0);
+    List<PictureDTO> actual = pictureServiceUnderTest.getPicturesForVehicle(
+      giveMeTestPictureDTO().getVehicleID()
+    );
+    PictureDTO actualPicture = actual.get(0);
 
-        List<PictureDTO> expected = new ArrayList<>();
-        expected.add(giveMeTestPictureDTO());
-        PictureDTO expectedPicture = expected.get(0);
+    List<PictureDTO> expected = new ArrayList<>();
+    expected.add(giveMeTestPictureDTO());
+    PictureDTO expectedPicture = expected.get(0);
 
-        assertNotNull(actual);
-        assertEquals(expectedPicture.getPictureID(), actualPicture.getPictureID());
-    }
+    assertNotNull(actual);
+    assertEquals(expectedPicture.getPictureID(), actualPicture.getPictureID());
+  }
 
-    @Test
-    public void whenDeletePictureByIdItShouldReturnEmptyRepo() {
-        pictureServiceUnderTest.deletePictureById(
-                giveMeTestPicture().getPictureID()
-        );
+  @Test
+  public void whenDeletePictureByIdItShouldReturnEmptyRepo() {
+    pictureServiceUnderTest.deletePictureById(
+      giveMeTestPicture().getPictureID()
+    );
 
-        verify(pictureRepository).deleteById(any());
-    }
+    verify(pictureRepository).deleteById(any());
+  }
 }
