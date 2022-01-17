56f1c58b05a81911e2f076ed4fe4f5f1be8cbc7e
Aaron Hissting <aaron-joachim.hissting@stud.uni-bamberg.de>
2022-01-17 16:30:53 +0100

    npm

--- swtcamper/src/main/java/swtcamper/backend/services/BookingService.java
+++ swtcamper/src/main/java/swtcamper/backend/services/BookingService.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
 import javafx.util.Pair;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
