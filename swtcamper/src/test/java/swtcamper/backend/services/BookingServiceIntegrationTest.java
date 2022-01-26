61e0a754bd330ee96d7a19d327db96f64d7f9ce2
Aaron Hissting <aaron-joachim.hissting@stud.uni-bamberg.de>
2022-01-26 16:51:44 +0100

    npm

--- swtcamper/src/test/java/swtcamper/backend/services/BookingServiceIntegrationTest.java
+++ swtcamper/src/test/java/swtcamper/backend/services/BookingServiceIntegrationTest.java
@@ -1,9 +1,13 @@
 package swtcamper.backend.services;
 
 import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
 import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,54 +20,48 @@ import swtcamper.backend.entities.Booking;
 import swtcamper.backend.repositories.BookingRepository;
 import swtcamper.backend.repositories.OfferRepository;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class BookingServiceIntegrationTest {
 
-    @Autowired
-    private BookingService bookingServiceUnderTest;
+  @Autowired
+  private BookingService bookingServiceUnderTest;
 
-    @Autowired
-    private BookingRepository bookingRepository;
+  @Autowired
+  private BookingRepository bookingRepository;
 
-    private static Stream<List<Booking>> provideBookingsForGetAllBookings() {
-        Booking booking1 = new Booking();
-        Booking booking2 = new Booking();
-        Booking booking3 = new Booking();
-        List<Booking> emptyList = new ArrayList<>();
-        List<Booking> oneBookingList = List.of(booking1);
-        List<Booking> twoBookingsList = List.of(booking1, booking2);
-        List<Booking> threeBookingsList = List.of(booking1, booking2, booking3);
-        return Stream.of(
-                emptyList,
-                oneBookingList,
-                twoBookingsList,
-                threeBookingsList
-        );
-    }
-    // ----- TESTING -----
+  private static Stream<List<Booking>> provideBookingsForGetAllBookings() {
+    Booking booking1 = new Booking();
+    Booking booking2 = new Booking();
+    Booking booking3 = new Booking();
+    List<Booking> emptyList = new ArrayList<>();
+    List<Booking> oneBookingList = List.of(booking1);
+    List<Booking> twoBookingsList = List.of(booking1, booking2);
+    List<Booking> threeBookingsList = List.of(booking1, booking2, booking3);
+    return Stream.of(
+      emptyList,
+      oneBookingList,
+      twoBookingsList,
+      threeBookingsList
+    );
+  }
 
-    @ParameterizedTest
-    @MethodSource("provideBookingsForGetAllBookings")
-    public void getAllBookingsShouldReturnAllBookings(List<Booking> expected) {
-        // arrange
-        bookingRepository.saveAll(expected);
-        // act
-        List<Booking> actual = bookingServiceUnderTest.getAllBookings();
-        // assert
-        assertEquals(expected, actual);
-    }
+  // ----- TESTING -----
 
-    @Test
-    public void test(){
-        assertEquals(true, true);
-    }
+  @ParameterizedTest
+  @MethodSource("provideBookingsForGetAllBookings")
+  public void getAllBookingsShouldReturnAllBookings(List<Booking> expected) {
+    // arrange
+    bookingRepository.saveAll(expected);
+    // act
+    List<Booking> actual = bookingServiceUnderTest.getAllBookings();
+    // assert
+    assertEquals(expected, actual);
+  }
 
+  @Test
+  public void test() {
+    assertEquals(true, true);
+  }
 }
