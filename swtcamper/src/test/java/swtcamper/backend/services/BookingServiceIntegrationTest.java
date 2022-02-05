3fca4dcc7e77f34806bd05787fae3f0ff6978fce
Aaron Hissting <aaron-joachim.hissting@stud.uni-bamberg.de>
2022-02-05 16:26:31 +0100

    temp

--- swtcamper/src/test/java/swtcamper/backend/services/BookingServiceIntegrationTest.java
+++ swtcamper/src/test/java/swtcamper/backend/services/BookingServiceIntegrationTest.java
@@ -4,10 +4,12 @@ import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.stream.Stream;
 import org.junit.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
@@ -21,9 +23,10 @@ import swtcamper.backend.entities.Booking;
 import swtcamper.backend.repositories.BookingRepository;
 import swtcamper.backend.repositories.OfferRepository;
 
-@RunWith(Parameterized.class)
+
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class BookingServiceIntegrationTest {
 
@@ -33,42 +36,26 @@ public class BookingServiceIntegrationTest {
   @Autowired
   private BookingRepository bookingRepository;
 
-  @Parameterized.Parameters
-  public static ArrayList<List> provideBookingsForGetAllBookings() {
-    Booking booking1 = new Booking();
-    Booking booking2 = new Booking();
-    Booking booking3 = new Booking();
-    List<Booking> emptyList = new ArrayList<>();
-    List<Booking> oneBookingList = List.of(booking1);
-    List<Booking> twoBookingsList = List.of(booking1, booking2);
-    List<Booking> threeBookingsList = List.of(booking1, booking2, booking3);
-    ArrayList<List> parameters = new ArrayList<>();
-    parameters.add(emptyList);
-    parameters.add(oneBookingList);
-    parameters.add(twoBookingsList);
-    parameters.add(threeBookingsList);
-    return parameters;
-  }
-
-  fun arrays()= listOf(listOf(listOf(1,2,3,4,5,6,7,8,9),3, listOf(4,6,8)))
-
-  fun arrays()= arrayOf(listOf(listOf(1,2,3,4,5,6,7,8,9),3, listOf(4,6,8)))
-
   // ----- TESTING -----
 
-  @ParameterizedTest
-  @MethodSource("provideBookingsForGetAllBookings")
-  public void getAllBookingsShouldReturnAllBookings(List<Booking> expected) {
+  @Test
+  public void getAllBookingsShouldReturnAllBookings() {
     // arrange
+    Booking booking = new Booking();
+    List<Booking> expected = new ArrayList<>();
+    expected.add(booking);
     bookingRepository.saveAll(expected);
     // act
     List<Booking> actual = bookingServiceUnderTest.getAllBookings();
     // assert
-    assertEquals(expected, actual);
+    Booking actualBooking = actual.get(0);
+    Booking expectedBooking = expected.get(0);
+    assertThat(actualBooking).usingRecursiveComparison()
+            .isEqualTo(expectedBooking);
   }
 
-  @Test
-  public void test() {
-    assertEquals(true, true);
-  }
+//  @Test
+//  public void test() {
+//    assertEquals(true, true);
+//  }
 }
