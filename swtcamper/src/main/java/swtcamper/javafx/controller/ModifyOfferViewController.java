eac3f442a0127bbdb6a0e7bc6738da085595f7f7
Thomas Kretschmann <t.kretschmann@t-online.de>
2022-02-07 15:17:13 +0100

    fixed bug that would not display rentalConditions

--- swtcamper/src/main/java/swtcamper/javafx/controller/ModifyOfferViewController.java
+++ swtcamper/src/main/java/swtcamper/javafx/controller/ModifyOfferViewController.java
@@ -316,7 +316,6 @@ public class ModifyOfferViewController implements EventHandler<KeyEvent> {
     fuelComboBox.setItems(
       FXCollections.observableArrayList((FuelType.values()))
     );
-    rentalConditionsListView = new ListView<>();
   }
 
   /**
